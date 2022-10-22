bbf70fa66cc77d32b61a553b4b422a750bbf5406
JoaoBebber <joao.gbebber@gmail.com>
2022-10-22 04:35:55 -0300

    :hammer: update: pius-paths

--- src/config/swagger/paths/piusPaths.ts
+++ src/config/swagger/paths/piusPaths.ts
@@ -20,6 +20,8 @@ const piusPaths: OpenAPIV3.PathsObject = {
               properties: {
                 content: {
                   type: 'string',
+                  minLength: 1,
+                  maxLength: 140,
                 },
               },
             },
@@ -39,6 +41,286 @@ const piusPaths: OpenAPIV3.PathsObject = {
         },
         400: {
           description: 'Bad Request',
+          content: {
+            'application/json': {
+              schema: {
+                properties: {
+                  status: {
+                    type: 'string',
+                  },
+                  message: {
+                    type: 'string',
+                  },
+                },
+              },
+            },
+          },
+        },
+        401: {
+          description: 'Unauthorized',
+          content: {
+            'application/json': {
+              schema: {
+                properties: {
+                  status: {
+                    type: 'string',
+                  },
+                  message: {
+                    type: 'string',
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    get: {
+      summary: 'Listar Pius',
+      description: 'Documentação de como listar todos os Pius.',
+      tags: ['Pius'],
+      security: [
+        {
+          bearerAuth: [],
+        },
+      ],
+      responses: {
+        200: {
+          description: 'OK',
+          content: {
+            'application/json': {
+              schema: {
+                type: 'array',
+                items: {
+                  type: 'object',
+                  properties: {
+                    id: {
+                      type: 'string',
+                    },
+                    authorId: {
+                      type: 'string',
+                    },
+                    content: {
+                      type: 'string',
+                    },
+                    created_at: {
+                      type: 'string',
+                    },
+                    updated_at: {
+                      type: 'string',
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+        401: {
+          description: 'Unauthorized',
+          content: {
+            'application/json': {
+              schema: {
+                properties: {
+                  status: {
+                    type: 'string',
+                  },
+                  message: {
+                    type: 'string',
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  '/pius/favorite': {
+    post: {
+      summary: 'Favoritar ou Desfavoritar Piu',
+      description: 'Documentação de como favoritar e desfavoritar um Piu. Note: Caso o Piu esteja favoritado, esse request irá desfavoritá-lo.',
+      tags: ['Pius'],
+      security: [
+        {
+          bearerAuth: [],
+        },
+      ],
+      requestBody: {
+        content: {
+          'application/json': {
+            schema: {
+              properties: {
+                piuId: {
+                  type: 'string',
+                },
+              },
+            },
+          },
+        },
+      },
+      responses: {
+        200: {
+          description: 'OK',
+          content: {
+            'application/json': {
+              schema: {
+                properties: {
+                  operation: {
+                    type: 'string',
+                  },
+                  piu: {
+                    type: 'object',
+                    properties: {
+                      id: {
+                        type: 'string',
+                      },
+                      authorId: {
+                        type: 'string',
+                      },
+                      content: {
+                        type: 'string',
+                      },
+                      created_at: {
+                        type: 'string',
+                      },
+                      updated_at: {
+                        type: 'string',
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+        400: {
+          description: 'Bad Request',
+          content: {
+            'application/json': {
+              schema: {
+                properties: {
+                  status: {
+                    type: 'string',
+                  },
+                  message: {
+                    type: 'string',
+                  },
+                },
+              },
+            },
+          },
+        },
+        401: {
+          description: 'Unauthorized',
+          content: {
+            'application/json': {
+              schema: {
+                properties: {
+                  status: {
+                    type: 'string',
+                  },
+                  message: {
+                    type: 'string',
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  '/pius/like': {
+    post: {
+      summary: 'Curtir ou Descurtir Piu',
+      description: 'Documentação de como curtir e descurtir um Piu. Note: Caso o Piu esteja curtido, esse request irá descurti-lo.',
+      tags: ['Pius'],
+      security: [
+        {
+          bearerAuth: [],
+        },
+      ],
+      requestBody: {
+        content: {
+          'application/json': {
+            schema: {
+              properties: {
+                piuId: {
+                  type: 'string',
+                },
+              },
+            },
+          },
+        },
+      },
+      responses: {
+        200: {
+          description: 'OK',
+          content: {
+            'application/json': {
+              schema: {
+                properties: {
+                  operation: {
+                    type: 'string',
+                  },
+                  piu: {
+                    type: 'object',
+                    properties: {
+                      id: {
+                        type: 'string',
+                      },
+                      authorId: {
+                        type: 'string',
+                      },
+                      content: {
+                        type: 'string',
+                      },
+                      created_at: {
+                        type: 'string',
+                      },
+                      updated_at: {
+                        type: 'string',
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+        400: {
+          description: 'Bad Request',
+          content: {
+            'application/json': {
+              schema: {
+                properties: {
+                  status: {
+                    type: 'string',
+                  },
+                  message: {
+                    type: 'string',
+                  },
+                },
+              },
+            },
+          },
+        },
+        401: {
+          description: 'Unauthorized',
+          content: {
+            'application/json': {
+              schema: {
+                properties: {
+                  status: {
+                    type: 'string',
+                  },
+                  message: {
+                    type: 'string',
+                  },
+                },
+              },
+            },
+          },
         },
       },
     },
