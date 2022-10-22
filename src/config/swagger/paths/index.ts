88c4d1aab01dc6525e156a71409db8b6e658c52b
JoaoBebber <joao.gbebber@gmail.com>
2022-10-22 03:17:17 -0300

    :tada: add: initial-tests-docs

--- src/config/swagger/paths/index.ts
+++ src/config/swagger/paths/index.ts
@@ -1,12 +1,14 @@
 import commentsPaths from './commentsPaths';
 import piusPaths from './piusPaths';
 import sessionsPaths from './sessionsPaths';
+import testsPaths from './testsPaths';
 import usersPaths from './usersPaths';
 
 const paths = {
   ...commentsPaths,
   ...piusPaths,
   ...sessionsPaths,
+  ...testsPaths,
   ...usersPaths,
 };
 
