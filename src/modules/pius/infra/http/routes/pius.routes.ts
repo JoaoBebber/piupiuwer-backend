848f851ebc8cc0f43eed537dc2e297d63607974b
JoaoBebber <joao.gbebber@gmail.com>
2022-10-22 01:11:23 -0300

    :tada: add: favorite-piu-route

--- src/modules/pius/infra/http/routes/pius.routes.ts
+++ src/modules/pius/infra/http/routes/pius.routes.ts
@@ -6,8 +6,14 @@ const piusRouter = Router();
 
 const piusController = new PiusController();
 
+// General Requests
 piusRouter.post('/', piusController.create);
 piusRouter.get('/', piusController.list);
+
+// Favorite Requests
+piusRouter.post('/favorite', piusController.favorite);
+
+// Like Requests
 piusRouter.post('/like', piusController.like);
 
 export default piusRouter;
