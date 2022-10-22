5d58df91d066c8ba77b8c7c8ef9a90e808a1a4bd
JoaoBebber <joao.gbebber@gmail.com>
2022-10-22 00:23:32 -0300

    :tada: add: pius-controller-like-method

--- src/modules/pius/infra/http/controllers/PiusController.ts
+++ src/modules/pius/infra/http/controllers/PiusController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import CreatePiuService from '@modules/pius/services/CreatePiuService';
+import LikePiuService from '@modules/pius/services/LikePiuService';
 import ListPiusService from '@modules/pius/services/ListPiusService';
 
 class PiusController {
@@ -13,6 +14,19 @@ class PiusController {
     const piu = await createPiu.execute({ authorId: req.user.id, content });
 
     return res.status(201).json(piu);
+  }
+
+  public async like(req: Request, res: Response): Promise<Response> {
+    const { piuId } = req.body;
+
+    const likePiu = container.resolve(LikePiuService);
+
+    const piu = await likePiu.execute({
+      piuId,
+      userId: req.user.id,
+    });
+
+    return res.json(piu);
   }
 
   public async list(_req: Request, res: Response): Promise<Response> {
