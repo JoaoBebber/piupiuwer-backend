fece43b81c480f6b1cbbe3700c55a59a70c7d35b
JoaoBebber <joao.gbebber@gmail.com>
2022-10-21 23:52:46 -0300

    :tada: add: pius-controller-list-method

--- src/modules/pius/infra/http/controllers/PiusController.ts
+++ src/modules/pius/infra/http/controllers/PiusController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import CreatePiuService from '@modules/pius/services/CreatePiuService';
+import ListPiusService from '@modules/pius/services/ListPiusService';
 
 class PiusController {
   public async create(req: Request, res: Response): Promise<Response> {
@@ -12,6 +13,14 @@ class PiusController {
     const piu = await createPiu.execute({ authorId: req.user.id, content });
 
     return res.status(201).json(piu);
+  }
+
+  public async list(_req: Request, res: Response): Promise<Response> {
+    const listPius = container.resolve(ListPiusService);
+
+    const pius = await listPius.execute();
+
+    return res.json(pius);
   }
 }
 
